50711c4fb60be7154a7f365046a46fd82747a1e8
Jane Volin <janevolin@yahoo.com>
2017-09-02 22:56:48 -0400

    fixed seed file

--- seed.js
+++ seed.js
@@ -1,17 +1,18 @@
 const db = require('./db');
-const Student = require('./db/models/student');
 const Campus = require('./db/models/campus');
+const Student = require('./db/models/student');
 
 const campuses = [
   {
     name: 'Mountainville Campus',
     photo: 'http://cdn.demo.fabthemes.com/kansas/files/2012/08/wallpaper-722590cp.jpg',
-    description: 'The air without is impregnated with raindew moisture, life essence celestial, glistening on Dublin stone there under starshiny coelum. God’s air, the Allfather’s air, scintillant circumambient cessile air. Breathe it deep into thee. By heaven, Theodore Purefoy, thou hast done a doughty deed and no botch! Thou art, I vow, the remarkablest progenitor barring none in this chaffering allincluding most farraginous chronicle'
+    description: 'The air without is impregnated with raindew moisture, life essence celestial, glistening on Dublin stone there under starshiny coelum. God’s air, the Allfather’s air, scintillant circumambient cessile air. Breathe it deep into thee. By heaven, Theodore Purefoy, thou hast done a doughty deed and no botch! Thou art, I vow, the remarkablest progenitor barring none in this chaffering allincluding most farraginous chronicle',
+
   },
   {
     name: 'Beachtown Campus',
     photo: 'http://images.kuoni.co.uk/73/solana-beach-34389580-1494493023-ImageGalleryLightboxLarge.jpg',
-    description: 'Ancient alien vortex helicopter heiroglyph elongated skull sightings, sanskrit mainstream archaelogy I know it sounds crazy…, Earth mound choral castle sky people. Mystery mainstream archaelogy Chariot of the Gods ancient god, otherworldly visitors burmuta triangle aircraft grey flying vessels inter-dimensional space travel, weightless electromagnetic pyramids extraterrestrial origin golden disk.'
+    description: 'Ancient alien vortex helicopter heiroglyph elongated skull sightings, sanskrit mainstream archaelogy I know it sounds crazy…, Earth mound choral castle sky people. Mystery mainstream archaelogy Chariot of the Gods ancient god, otherworldly visitors burmuta triangle aircraft grey flying vessels inter-dimensional space travel, weightless electromagnetic pyramids extraterrestrial origin golden disk.',
   },
   {
     name: 'Open Road Remote Campus',
@@ -26,35 +27,38 @@ const students = [
     email: 'sal@paradise.com',
     photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-5.jpg',
     bio: 'Vet chihuahua play dead husky come leash pomeranian dachshund. Down maltese stay great dance great dance leave it chow chow poodle leash. Release lab poodle fetch st bernard leave it pit bull squirrel fetch, dog bone sit pretty dog fetch. Dog house tennis ball vet pomsky roll over shih tzu, release beagle bring it fetch bite vet kong. Bang squeak toy sit pretty pomeranian spin, husky tail dog bone milk bone st bernard.',
+    campusId: 1,
   },
   {
     name: 'Dean Moriarty',
     email: 'dean@moriarty.com',
     photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjyx_mqq4TWAhVKxoMKHTKcCRMQjRwIBw&url=http%3A%2F%2Fdogtime.com%2Fpuppies%2F255-puppies&psig=AFQjCNE3rjH02p_S2mMaWswot4iC7l5Jzg&ust=1504367021813189',
     bio: 'Vet chihuahua play dead husky come leash pomeranian dachshund. Down maltese stay great dance great dance leave it chow chow poodle leash. Release lab poodle fetch st bernard leave it pit bull squirrel fetch, dog bone sit pretty dog fetch. Dog house tennis ball vet pomsky roll over shih tzu, release beagle bring it fetch bite vet kong. Bang squeak toy sit pretty pomeranian spin, husky tail dog bone milk bone st bernard.',
+    campusId: 1,
   },
   {
     name: 'Carlo Marx',
     email: 'carlo@marx.com',
-    photo:'http://cdn.skim.gs/image/upload/v1456344012/msi/Puppy_2_kbhb4a.jpg',
+    photo: 'http://cdn.skim.gs/image/upload/v1456344012/msi/Puppy_2_kbhb4a.jpg',
     bio: 'Sit stand catch rottweiler yorkshire terrier, bite chihuahua puppy tennis ball speak beagle sit. Bulldog sit doberman pinscher vet yorkshire terrier poodle, take it release roll over lap dog. Bring it release beagle rottweiler dog bone leap shake. Bang english mastiff puppy bang pit bull speak poodle, tail bulldog play dead dog toy jump. Squirrel pit bull pomeranian maltese bang k9 down stand.',
+    campusId: 2,
   },
   {
     name: 'Galatea Dunkel',
     email: 'Galetea@dunkel.com',
     photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjRsLzzq4TWAhWiy4MKHcDsD_gQjRwIBw&url=https%3A%2F%2Fwww.pinterest.com%2Fexplore%2Fpuppies%2F&psig=AFQjCNE3rjH02p_S2mMaWswot4iC7l5Jzg&ust=1504367021813189',
     bio: 'Greyhound bite pomeranian puppies english mastiff chew toy play dead, sit pretty chew toy chew toy kong lap dog bang. Dog bone st bernard dachshund milk bone puppies pit bull, poodle pomsky lap dog pomeranian dog house. Puppies vet pomeranian st bernard stay doberman pinscher st bernard, dog toy lab chow chow leave it bang pomsky release.',
+    campusId: 3,
   },
 ]
 
-
 const seed = () =>
-  Promise.all(students.map(student =>
-    Student.create(student))
-  )
-  .then(() =>
   Promise.all(campuses.map(campus =>
     Campus.create(campus))
+  )
+  .then(() =>
+  Promise.all(students.map(student =>
+    Student.create(student))
   ))
 ;
 
